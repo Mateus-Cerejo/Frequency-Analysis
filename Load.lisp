a66bc270f4cc77716d297d7bd06395b6c6816574
ludef4r <luis.faria.martins@gmail.com>
2024-07-14 15:09:23 +0100

    Beginning of the project
    Added text to decipher.
    Function to get the path to the file
    And test to make sure everything is right

---
+++ Load.lisp
@@ -0,0 +1,6 @@
+(defun get-path(file)
+  "Returns the path to the file"
+  (concatenate 'string "~/Projects/SI-Project/" file))
+
+(defun load-data(pathname)
+  pathname)
---
+++ tests/Load-Tests.lisp
@@ -0,0 +1,13 @@
+(defun test-get-path()
+  "Checks if the path is correct"                                                                       
+  (let* ((file "Test.lisp")                                                                             
+        (expectedPath (concatenate 'string "~/Projects/SI-Project/" file)))                             
+    (cond ((string= expectedPath (get-path file)) (format t "Passed Test - Get-Path"))
+          (t (format t "Failed to get the correct path~%")
+	     (format t "Expected ~a~%" expectedPath)
+	     (format t "Value ~a~%" (get-path file))))))
+
+
+(defun tests()
+  "Run all tests"
+  (test-get-path))
