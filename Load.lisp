baf92bc2c556a4cacdeec0c1b756975189f64014
ludef4r <luis.faria.martins@gmail.com>
2024-07-15 17:00:12 +0100

    Some refactor and corrections to the code
    Small corrections to the code
    And refactor so that is more readable and maintanable
    Note: Tests still need some correction

--- Load.lisp
+++ Load.lisp
@@ -2,24 +2,30 @@
   "Returns the path to the file"
   (concatenate 'string "~/Projects/SI-Project/" file))
 
+(defun is-alphabet(char)
+  "Checks if char is a letter from the alphabet"
+  (or (and (char>= char #\A) (char<= char #\Z))
+      (and (char>= char #\a) (char<= char #\z))))
+
+(defun set-map()
+  "Loads the information needed to the map.
+   The keys in the map will be the characters and the values will be the letter's frequency."
+  (loop for character across text
+	do (let ((value (gethash character frequency-table))
+		 (is-alpha (or (is-alphabet character) (char= character #\�))))
+	     (cond ((and is-alpha (null value)) (setf value 1))
+		   (is-alpha (setf value (1+ value))))
+	     (if is-alpha (setf (gethash character frequency-table) value))))
+  (let ((total 0))
+    (maphash (lambda (_ value) (setf total (+ total value))) frequency-table)
+    (get-percentage total)))
+
 (defun load-data(pathname)
   "Gets ciphered text and stores the frequency of each letter in frequency table"
-  (let ((total 0))
-    (with-open-file (stream pathname
-			    :direction :input
-			    :if-does-not-exist :error)
-      (when stream
-	(loop for character = (read-char stream nil nil)
-	      while character do
-		(setf text (concatenate 'string text (string character)))
-		(let ((value (gethash character frequency-table))
-		      (is-alphabet (or (and (char>= character #\A) (char<= character #\Z))
-				       (and (char>= character #\a) (char<= character #\z))
-				       (char= character #\�))))
-		  (cond ((and (null value) is-alphabet)
-			 (setf (gethash character frequency-table) 1)
-			 (setf total (1+ total)))
-			(is-alphabet
-			 (setf (gethash character frequency-table) (+ value 1))
-			 (setf total (1+ total))))))))
-    total))
+  (with-open-file (stream pathname
+			  :direction :input
+			  :if-does-not-exist :error)
+    (when stream
+      (loop for character = (read-char stream nil nil)
+	    while character do
+	      (setf text (concatenate 'string text (string character)))))))
