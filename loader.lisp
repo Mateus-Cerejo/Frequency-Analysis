c2835210edcb64f5a455e9f92e2ba43827e0225b
ludef4r <luis.faria.martins@gmail.com>
2024-07-22 10:47:38 +0100

    Correction to load of the project
    Now it makes sure to put all compiled files in compiled folder

--- loader.lisp
+++ loader.lisp
@@ -1,10 +1,29 @@
+;;; Package
+
 (in-package :loader-project)
+
+;;; Constants
+
+(defconstant +compiled-dir+ (merge-pathnames "compiled/" (truename ".")))
+(defconstant +compiled-extension+ "fasl")
+
+;;; Functions
+
+(defun compile-and-load(source)
+  "Compile and load the source file, storing the compiled file in the specified directory."
+  (ensure-directories-exist +compiled-dir+)
+  (let ((compiled (merge-pathnames (make-pathname :name (pathname-name source)
+						  :type +compiled-extension+
+						  :version (pathname-version source))
+				   +compiled-dir+)))
+    (compile-file source :output-file compiled)
+    (load compiled)))
 
 (defun load-all-files()
   "Loads all the necessary files in the project, in correct order"
-  (load (compile-file "src/scripts/utils/path.lisp"))
-  (load (compile-file "src/scripts/utils/character.lisp"))
-  (load (compile-file "src/scripts/utils/number.lisp"))
-  (load (compile-file "src/scripts/utils/table.lisp"))
-  (load (compile-file "src/scripts/load.lisp"))
-  (load (compile-file "src/main.lisp")))
+  (compile-and-load "src/scripts/utils/path.lisp")
+  (compile-and-load "src/scripts/utils/character.lisp")
+  (compile-and-load "src/scripts/utils/number.lisp")
+  (compile-and-load "src/scripts/utils/table.lisp")
+  (compile-and-load "src/scripts/load.lisp")
+  (compile-and-load "src/main.lisp"))
