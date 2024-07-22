6ceabae8d9ce08dd3e389d3ee9151c8711f5a5a4
ludef4r <luis.faria.martins@gmail.com>
2024-07-22 15:23:33 +0100

    helpers.lisp documentation

--- src/scripts/utils/helpers.lisp
+++ src/scripts/utils/helpers.lisp
@@ -1,3 +1,22 @@
+;;;; Utility Functions
+
+;;;; This file contains utility functions used throughout the project. It includes:
+;;;;
+;;;;     - Path Functions:
+;;;;         - `get-path-to-files`: Returns the full path to a file, optionally including a folder.
+;;;;     - Constant Functions:
+;;;;         - `ensure-constant`: Initializes a constant if it doesn't already exist.
+;;;;
+;;;; Usage:
+;;;;     These utility functions are meant to be used by other parts of the project 
+;;;;     to perform common operations. The `get-path-to-files` function, for example, 
+;;;;     is used to construct file paths dynamically, and the `ensure-constant` function 
+;;;;     ensures that constants are defined safely.
+;;;;
+;;;; Note:
+;;;;     This file assumes the `utils` package is properly defined and used across 
+;;;;     the project.
+
 ;;; Package
 
 (in-package :utils)
