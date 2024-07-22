6d836107925eeaa46d488c05f24be5bf8197b118
ludef4r <luis.faria.martins@gmail.com>
2024-07-22 15:21:45 +0100

    character.lisp documentation

--- src/scripts/utils/character.lisp
+++ src/scripts/utils/character.lisp
@@ -1,3 +1,19 @@
+;;;; Utility Functions
+
+;;;; This file contains utility functions used throughout the project. It includes:
+;;;;
+;;;;     - Character Functions:
+;;;;         - `alphabetp`: Checks if a character is a letter from the alphabet.
+;;;;
+;;;; Usage:
+;;;;     These utility functions are meant to be used by other parts of the project 
+;;;;     to perform common operations. The `alphabetp` function, for example, is used 
+;;;;     to determine if a character is alphabetic.
+;;;;
+;;;; Note:
+;;;;     This file assumes the `utils` package is properly defined and used across 
+;;;;     the project.
+
 ;;; Package
 
 (in-package :utils)
